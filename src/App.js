79c3428590b3edae577df029d3ca05206e72d907
Santiago Enciso <santiago.enciso@q2.com>
2021-09-14 20:38:55 -0500

    Implemented Switch into Router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { render } from "react-dom";
 import { StrictMode } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
       <Router>
-        <Route path="/details/:id">
-          <Details />
-        </Route>
-        <Route path="/">
-          <SearchParams />
-        </Route>
+        <h1>Adopt Me!</h1>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
       </Router>
-      ;
     </div>
   );
 };
