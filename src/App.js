82b66c1a64b1d1a7d914c4b03b9246d6e188ff5b
Santiago Enciso <santiago.enciso@q2.com>
2021-09-14 19:23:31 -0500

    Added Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { render } from "react-dom";
 import { StrictMode } from "react";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <Route path="/details/:id">
+          <Details />
+        </Route>
+        <Route path="/">
+          <SearchParams />
+        </Route>
+      </Router>
+      ;
     </div>
   );
 };
