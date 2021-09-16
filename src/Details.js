e5c61b459c5dd7eda47862171a836da92b9d7137
Santiago Enciso <santiago.enciso@q2.com>
2021-09-16 08:52:06 -0500

    replaced setState props with Object.assign

--- src/Details.js
+++ src/Details.js
@@ -10,15 +10,7 @@ class Details extends Component {
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
     );
     const json = await res.json();
-    this.setState({
-      loading: false,
-      name: json.pets[0].name,
-      breed: json.pets[0].breed,
-      animal: json.pets[0].animal,
-      city: json.pets[0].city,
-      state: json.pets[0].state,
-      description: json.pets[0].description,
-    });
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
 
   render() {
