8b7a8cd54d3920116c0d46bb5f71f74e7cbb7c01
Santiago Enciso <santiago.enciso@q2.com>
2021-09-17 08:59:30 -0500

    Created ErrorBoundary component and added it to Details.js

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErroBoundary";
 
 class Details extends Component {
   state = { loading: true };
@@ -35,4 +36,12 @@ class Details extends Component {
   }
 }
 
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter {...props} />
+    </ErrorBoundary>
+  );
+}
