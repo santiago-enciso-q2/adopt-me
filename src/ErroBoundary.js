efc1864a89773f2ad42ea5a5a1ca8fb28380e3d7
Santiago Enciso <santiago.enciso@q2.com>
2021-09-18 09:55:41 -0500

    Added redirect to homepage after 5 seconds if user makes no action

--- src/ErroBoundary.js
+++ src/ErroBoundary.js
@@ -1,16 +1,20 @@
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
   state = { hasError: false };
   static getDerivedStateFromError() {
-    return { hasError: true };
+    return { hasError: true, redirect: false };
   }
   componentDidCatch(error, info) {
     console.error("ErrorBoundary caught an error", error, info);
+    setTimeout(() => this.setState({ redirect: true }), 5000);
   }
+
   render() {
-    if (this.state.hasError) {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    } else if (this.state.hasError) {
       return (
         <h2>
           There was an error with this listing. <Link to="/">Click here</Link>{" "}
